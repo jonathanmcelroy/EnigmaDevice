d9779b36bcd1652323286274dec1549542ea1a11
EliseBrown <Elise_Brown@baylor.edu>
2013-12-02 18:33:06 -0600

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,14 +1,11 @@
 #ifndef CONSTANTS_H_INCLUDED
 #define CONSTANTS_H_INCLUDED
 
-//a set of constants used throughout the game
 
 //the dimentions of the screen
 const unsigned int SCREENWIDTH = 160;
+
 const unsigned int SCREENHEIGHT = 50;
-//the amount of ammo at the start
-const int AMMO = 20;
-//the amount of enemy missiles on the screen at once
-const int SET_NUM_MISSILES = 7;
+
 
 #endif // CONSTANTS_H_INCLUDED
