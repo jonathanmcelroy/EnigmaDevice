c1f1e7c6ba9c23fec88d36f6e0bb96d9aef485e7
EliseBrown <Elise_Brown@baylor.edu>
2013-12-03 13:03:16 -0600

    header constants.h

--- constants.h
+++ constants.h
@@ -1,7 +1,8 @@
 /*
 	Authors: Jonathan McElroy, Aaron Salinas, Elise Brown
-	FileName:
-	Description:
+	FileName: constants.h
+	Description: simple constants that determine the dimensions
+	of the screen
 */
 #ifndef CONSTANTS_H_INCLUDED
 #define CONSTANTS_H_INCLUDED
